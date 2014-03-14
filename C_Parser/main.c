c21089f129b8eda85fd6b30147491a62e1a36d8e
Harshit Sanghvi <harshit@Harshits-MacBook-Air.local>
2014-03-14 19:35:46 -0400

    Initial Commit

---
+++ C_Parser/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  C_Parser
+//
+//  Created by Harshit Sanghvi on 3/14/14.
+//  Copyright (c) 2014 Harshit. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
